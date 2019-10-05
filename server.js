ee4c9ca9a93882e183c12b4ac92f11e0176615bd
AlaCrity <wlchen1105@gmail.com>
2019-10-04 23:28:19 -0400

    set up of files and connection to the db

--- server.js
+++ server.js
@@ -1,37 +1,28 @@
 var express = require("express");
-var exphbs = require("express-handlebars");
+var logger = require('morgan"');
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
 var PORT = 8000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
-var MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-app.get("/", function(req, res) {
-  res.send("route hit");
-});
+console.log("Connected to Mongoose");
 
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
   axios.get("http://www.allkpop.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
