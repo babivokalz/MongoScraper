eeba365e09db774bb4aff314e0c3e1602737cc31
AlaCrity <wlchen1105@gmail.com>
2019-10-06 21:47:44 -0400

    made changes to the server.js file to use express handlebars

--- models/Article.js
+++ models/Article.js
@@ -12,10 +12,12 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
-  comments: {
-    type: Schema.Types.ObjectId,
-    ref: "Comment"
-  }
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
 
 // This creates our model from the above schema, using mongoose's model method
